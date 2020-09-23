0d2ac1df69061edee7ddc68dad7beb0cab3f5f90
Davide Cesare <davidereaper@gmail.com>
2020-09-23 15:01:44 +0200

    Added flag (da fixare)

--- js/script.js
+++ js/script.js
@@ -25,27 +25,44 @@ function tempMovie(dataRes) {
   var template = Handlebars.compile(source);
 
   for (var i = 0; i < dataRes.length; i++) {
-    var vote = dataRes[i].vote_average;
-    
+    var language = langEnglish(dataRes[i].original_language);
+    var vote = voteMovieMath(dataRes[i].vote_average);
+
     var context = {
       "title" : dataRes[i].title,
       "original_title" : dataRes[i].original_title,
-      "language" : dataRes[i].original_language,
-      "vote" : voteMovieMath(vote)
+      "language" : language,
+      "vote" : vote
     };
     var html = template(context);
     $("#movie_list").append(html);
   }
 }
 
+function langEnglish(language) {
+  if ((language === "it") || (language === "de") || (language === "fr") || (language === "es") || (language === "pt") || (language === "ar")) {
+    $(".flag_img").html("<img src='https://www.countryflags.io/"+language+"/flat/32.png'>");
+  } else if (language === "en") {
+    $(".flag_img").html("<img src='https://www.countryflags.io/gb/flat/32.png'>");
+  } else {
+    $(".flag_img").html(language);
+  }
+}
+
 function voteMovieMath(voteOfI) {
   var numbVote = parseInt(Math.ceil(voteOfI));
   var voteFinal = parseInt(Math.ceil(numbVote / 2));
-  console.log(voteFinal);
+  // for (var j = 0; j < $(".vote_list li>i"); j++) {
+  //   $(".vote_list li>i").addClass("star_yellow");
+  //   $(".vote_list li>i").next();
+  // }
+  return voteFinal;
 }
 
 function searchMovie() {
   var search = $(".search_movie").val().toLowerCase();
+  // "https://api.themoviedb.org/3/search/movie"
+  // "https://api.themoviedb.org/3/search/tv"
   $.ajax(
     {
       "url" : "https://api.themoviedb.org/3/search/movie",
