f4220bb441af31187ba81524dad6f0d6f0823d79
Davide Cesare <davidereaper@gmail.com>
2020-09-23 11:49:39 +0200

    Inizio milestone 2

--- js/script.js
+++ js/script.js
@@ -19,28 +19,36 @@ $(document).ready(function(){
 
 });
 
-function tempMovie(dataR) {
+function tempMovie(dataRes) {
   $("#movie_list").html("");
   var source = $("#movie_template").html();
   var template = Handlebars.compile(source);
 
-  for (var i = 0; i < dataR.length; i++) {
+  for (var i = 0; i < dataRes.length; i++) {
+    var vote = dataRes[i].vote_average;
+    
     var context = {
-      "title" : dataR[i].title,
-      "original_title" : dataR[i].original_title,
-      "language" : dataR[i].original_language,
-      "vote" : dataR[i].vote_average
+      "title" : dataRes[i].title,
+      "original_title" : dataRes[i].original_title,
+      "language" : dataRes[i].original_language,
+      "vote" : voteMovieMath(vote)
     };
     var html = template(context);
     $("#movie_list").append(html);
   }
 }
 
+function voteMovieMath(voteOfI) {
+  var numbVote = parseInt(Math.ceil(voteOfI));
+  var voteFinal = parseInt(Math.ceil(numbVote / 2));
+  console.log(voteFinal);
+}
+
 function searchMovie() {
   var search = $(".search_movie").val().toLowerCase();
   $.ajax(
     {
-      "url" : "https://api.themoviedb.org/3/search/movie/",
+      "url" : "https://api.themoviedb.org/3/search/movie",
       "data" : {
         "api_key" : "8c29c10b1dce143e07a26dc3b69c6abc",
         "query" : search,
